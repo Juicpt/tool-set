765f58cad2212a798d2328cf914960ada5c71523
Juicpt <wdxpn@hotmail.com>
2020-07-10 14:31:11 +0800

    feat:依赖更新 fix:在app.server.module引入全量图标，解决打包报错警告  https://github.com/NG-ZORRO/ng-zorro-universal-starter/issues/4#issuecomment-533991292

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -6,13 +6,23 @@ import {AppComponent} from './app.component';
 import {RouterModule, Routes} from '@angular/router';
 import {AppShellComponent} from './app-shell/app-shell.component';
 
+import * as AllIcons from '@ant-design/icons-angular/icons';
+import {IconDefinition} from '@ant-design/icons-angular';
+import {NzIconModule} from 'ng-zorro-antd';
+
 const routes: Routes = [{path: 'shell', component: AppShellComponent}];
+
+const antDesignIcons = AllIcons as {
+  [key: string]: IconDefinition;
+};
+const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key]);
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
     RouterModule.forRoot(routes),
+    NzIconModule.forRoot(icons)
   ],
   bootstrap: [AppComponent],
   declarations: [AppShellComponent],
